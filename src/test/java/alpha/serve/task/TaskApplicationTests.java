eb2261e17452e5c0f6cdba812539c5f80969c924
jlightgreen <ShvabovichJulia@gmail.com>
2020-11-25 17:18:00 +0300

    first commit

---
+++ src/test/java/alpha/serve/task/TaskApplicationTests.java
@@ -0,0 +1,13 @@
+package alpha.serve.task;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TaskApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
